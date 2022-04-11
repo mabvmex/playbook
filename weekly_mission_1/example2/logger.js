b959513a9bcffdc4c7dc1fb636ba1ee9857395dc
mabvmex <mabvmex@gmail.com>
2022-04-11 16:44:32 -0500

    ejecicios completados

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,21 @@
+// logger.js
+
+// Esta es una función que se guardara en este módulo como info
+exports.info = (message) => {
+  console.log(`info: ${message}`)
+}
+
+// Esta es una función que se guardara en este módulo como verbose
+exports.verbose = (message) => {
+  console.log(`verbose: ${message}`)
+}
+
+exports.miVariable = (text) => {
+	console.log(`miVariable: ${text}` )
+}
+
+/*
+  const logger = require('./logger')
+  logger.info('This is an informational message')
+  logger.verbose('This is a verbose message')
+ * */
---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,18 @@
+const Pokemon = require('./pokemon')
+
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const charmander = new Pokemon("charmander")
+const squirtle = new Pokemon("squirtle")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
