08b0f44a0f86392156a2ea29c01b39fdbe29ae4a
mabvmex <mabvmex@gmail.com>
2022-04-27 11:11:06 -0500

    folder exercises relocation

--- weekly_mission_2/operadores/1.4_operador_map.js
+++ weekly_mission_2/operadores/1.4_operador_map.js
@@ -4,8 +4,8 @@ const modifiedArray = arr.map((element,index) => element);
 */
 
 const numbers4 = [1, 2, 3, 4, 5]
-const numbersSquare = numbers4.map(function(num){ return num * num})
+// const numbersSquare = numbers4.map(function(num){ return num * num})
 // También puedes escribir la función como fat arrow
-//const numbersSquare = numbers4.map((num) => return num * num)
+const numbersSquare = numbers4.map(num => {return num * num})
 console.log("Ejemplo 4: Imprimiendo la lista de elementos al cuadrado")
 console.log(numbersSquare)
