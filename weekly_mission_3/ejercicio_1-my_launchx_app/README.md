a74a755b9cb47c985e08fdfaf0002a4ba716fe15
mabvmex <mabvmex@gmail.com>
2022-04-28 14:59:26 -0500

    Semana 3 - Renaming > Ejercicio 1

---
+++ weekly_mission_3/ejercicio_1-my_launchx_app/README.md
@@ -0,0 +1,2 @@
+# Ejericicio 3.1 - LaunchX - NodeJS
+## Esquema básico de un proyecto JS con pruebas.
---
+++ weekly_mission_3/ejercicio_1-my_launchx_app/index.js
@@ -0,0 +1,4 @@
+const MissionCommander = require('./app/missionComander');
+
+const carlo = new MissionCommander('Carlo Gilmar')
+console.log(carlo);
