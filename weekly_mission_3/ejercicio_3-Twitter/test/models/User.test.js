339a783c58530ed2485755afeccce92959b10949
mabvmex <mabvmex@gmail.com>
2022-04-30 15:17:36 -0500

    Semana 3 - Ejercicio 3-Twitter > Requerimiento-1: models/User.js y prueba completa

---
+++ weekly_mission_3/ejercicio_3-Twitter/test/models/User.test.js
@@ -0,0 +1,22 @@
+const User = require("./../../app/models/User");
+
+describe("UNIT TESTS FOR USER CLASS", () => {
+  test("Create an User object", () => {
+    const user = new User(
+      1,
+      "juanito-pistolas",
+      "Juan López",
+      "Vibrando Alto",
+      "dateCreated",
+      "lastUpdate"
+    );
+
+    expect(user.id).toBe(1)
+    expect(user.username).toBe('juanito-pistolas')
+    expect(user.name).toBe('Juan López')
+    expect(user.bio).toBe('Vibrando Alto')
+    expect(user.dateCreated).toBe('dateCreated')
+    expect(user.lastUpdate).toBe('lastUpdate')
+    
+  });
+});
