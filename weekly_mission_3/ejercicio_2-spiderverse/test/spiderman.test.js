e1795ce0728e2957854d179bbc8f4d6141df9fb9
mabvmex <mabvmex@gmail.com>
2022-04-30 05:07:01 -0500

    Se implementa prueba => falla comprobada por falta de clase creada

--- weekly_mission_3/ejercicio_2-spiderverse/test/spiderman.test.js
+++ weekly_mission_3/ejercicio_2-spiderverse/test/spiderman.test.js
@@ -1,6 +1,11 @@
-describe('Test Suite Dummy Description', () => {
-  test('Case 1 Dummy', () => {
-    const resultOfSomething = 1 + 2;
-    expect(resultOfSomething).toBe(3);
+describe('Unit Test for spiderman Class', () => {
+  test('1). Create a spiderman object', () => {
+
+    const andrewGarfield = new Spiderman("Spiderman Sony", 31, "andrew Garfield", 2, "Sony")
+    expect(andrewGarfield.name).toBe('Spiderman Sony');
+    expect(andrewGarfield.age).toBe(31);
+    expect(andrewGarfield.actor).toBe('andrew Garfield');
+    expect(andrewGarfield.totalMovies).toBe(2);
+    expect(andrewGarfield.studio).toBe('Sony');
   });
 });
